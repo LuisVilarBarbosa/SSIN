7456b0f2ff6727ccb8fc5da22607c32f6b6b711d
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2018-05-16 19:39:47 +0100

    Order of the modes changed to be the implementation order

--- 1-DNSAttackJava/Main.java
+++ 1-DNSAttackJava/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
         if (args.length < 3 || args.length > 4) {
             System.out.println("Usage: Main <mode_number> <timeout_millis> <erroneous_ip>");
-            System.out.println("- Mode 1: send fake answer to the first sniffed query.");
-            System.out.println("- Mode 2: send fake answer to a host that queries this machine.");
+            System.out.println("- Mode 1: send fake answer to a host that queries this machine.");
+            System.out.println("- Mode 2: send fake answer to the first sniffed query.");
             System.out.println("This program was designed only for IPv4");
             return;
         }
@@ -13,9 +13,9 @@ public class Main {
         final int timeoutMillis = Integer.parseInt(args[1]);
         final String erroneousIP = args[2];
         if (mode == 1)
-            new DNSAttack().sendFakeAnswerToSniffedQuery(timeoutMillis, erroneousIP);
-        else if (mode == 2)
             new DNSAttack().sendFakeAnswerToHost(timeoutMillis, erroneousIP);
+        else if (mode == 2)
+            new DNSAttack().sendFakeAnswerToSniffedQuery(timeoutMillis, erroneousIP);
         else
             System.out.println("Invalid mode.");
         System.exit(goodExitStatus);
--- 2-DNSAttackCSharp/SSIN/MainClass.cs
+++ 2-DNSAttackCSharp/SSIN/MainClass.cs
@@ -11,25 +11,25 @@ namespace DNS_Attack {
         public static void Main(string[] args) {
             if (args.Length < 3 || args.Length > 4) {
                 Console.WriteLine("Usage: Main <mode_number>...");
-                Console.WriteLine("Usage: Main 1 <timeout_millis> <erroneous_ip>");
-                Console.WriteLine("Usage: Main 2 <fake_source_ip> <timeout_millis> <erroneous_ip>");
-                Console.WriteLine("- Mode 1: send fake answer to the first sniffed query (not implemented).");
-                Console.WriteLine("- Mode 2: send fake answer to a host that queries this machine.");
+                Console.WriteLine("Usage: Main 1 <fake_source_ip> <timeout_millis> <erroneous_ip>");
+                Console.WriteLine("Usage: Main 2 <timeout_millis> <erroneous_ip>");
+                Console.WriteLine("- Mode 1: send fake answer to a host that queries this machine.");
+                Console.WriteLine("- Mode 2: send fake answer to the first sniffed query (not implemented).");
                 Console.WriteLine("This program was designed only for IPv4");
                 return;
             }
             try {
                 int mode = int.Parse(args[0]);
                 if (mode == 1) {
-                    int timeoutMillis = int.Parse(args[1]);
-                    string erroneousIP = args[2];
-                    new DNSAttack().SendFakeAnswerToSniffedQuery(timeoutMillis, erroneousIP);
-                }
-                else if (mode == 2) {
                     string fakeSourceIP = args[1];
                     int timeoutMillis = int.Parse(args[2]);
                     string erroneousIP = args[3];
                     new DNSAttack().SendFakeAnswerToHost(fakeSourceIP, timeoutMillis, erroneousIP);
+                }
+                else if (mode == 2) {
+                    int timeoutMillis = int.Parse(args[1]);
+                    string erroneousIP = args[2];
+                    new DNSAttack().SendFakeAnswerToSniffedQuery(timeoutMillis, erroneousIP);
                 }
                 else
                     Console.WriteLine("Invalid mode.");
